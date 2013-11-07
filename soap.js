a90328173b4ee4c6b56ef0c3576145f1395e088b
Alex Taylor <alexmckenley@gmail.com>
2013-11-06 17:49:05 -0800

    Cleaned up whitespace

--- soap.js
+++ soap.js
@@ -8,20 +8,19 @@ soap.createClient(url, function(err, client) {
   client.setSecurity(new soap.WSSecurity('mckenley1', 'doi/zyq5OvM+riRcaYhnJnpyQt7FD7WlF1QBQT0SMrhwN9rUKAeG7owGCb01PCyOGxNUIqvDrNiQItgl8nyBIfeNyo0JmH1F4DMlbKiIIc85EXEt2NlDkLvK0Do+hf2Bh97dTGFpL7FvVgUubuBjoyxhh8xh4PBvgomsFZuk+3t4C5nfPzplJLAkBWG2UfjSFAzcKZXeevl3pnR02NuDOpZbIXy8h/5dTI0D+KpM+WiYbJuUoiiKWPfmJmNq7ugCN12ruhTrkZp/YgeOU39AuOrdMCmzNS1TFDJZ60vf6W5ZPL5QN8WjFjUEbXDklOcwja7ylL1pVh8XS45CTqtOAg=='));
   
   //run transaction
-  client.runTransaction({ //fs.readFileSync('auth.xml', 'utf8')
-    merchantID: "mckenley1",
-    merchantReferenceCode: "NODEJS-REVERSE",
-    purchaseTotals:{
-      currency: "USD",
-      grandTotalAmount: 4.00
-    },
-    ccAuthReversalService: {
-      _run: true,
-      authRequestID: "3837883164250176056166"
-    }
-
-}
-    , function(err, result) {
+  client.runTransaction( //fs.readFileSync('auth.xml', 'utf8') //to read from file
+    {
+      merchantID: "mckenley1",
+      merchantReferenceCode: "NODEJS_FTW",
+      purchaseTotals: {
+        currency: "USD",
+        grandTotalAmount: 4.00
+      },
+      ccAuthReversalService: {
+        _run: true,
+        authRequestID: "3837883164250176056166"
+      }
+    }, function(err, result) {
       //console.log(err);
       console.log(result);
     });
