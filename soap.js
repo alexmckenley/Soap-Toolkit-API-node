4d8864b9306837a409b7b40baf08d969c9069d7a
Alex's Work Computer <alexmckenley@gmail.com>
2013-12-18 11:08:04 -0800

    Replaced grandTotalAmount with item_0_amount

--- soap.js
+++ soap.js
@@ -10,24 +10,30 @@ soap.createClient(url, function(err, client) {
   client.setSecurity(new soap.WSSecurity('mckenley1', 'doi/zyq5OvM+riRcaYhnJnpyQt7FD7WlF1QBQT0SMrhwN9rUKAeG7owGCb01PCyOGxNUIqvDrNiQItgl8nyBIfeNyo0JmH1F4DMlbKiIIc85EXEt2NlDkLvK0Do+hf2Bh97dTGFpL7FvVgUubuBjoyxhh8xh4PBvgomsFZuk+3t4C5nfPzplJLAkBWG2UfjSFAzcKZXeevl3pnR02NuDOpZbIXy8h/5dTI0D+KpM+WiYbJuUoiiKWPfmJmNq7ugCN12ruhTrkZp/YgeOU39AuOrdMCmzNS1TFDJZ60vf6W5ZPL5QN8WjFjUEbXDklOcwja7ylL1pVh8XS45CTqtOAg=='));
   
   //run transaction
-  for(var i = 5; i > 0; i--){
+  for(var i = 1; i > 0; i--){
   client.runTransaction( //fs.readFileSync('auth.xml', 'utf8') //to read from file
     {
       merchantID: "mckenley1",
-      merchantReferenceCode: "NODEJS_FTW",
+      merchantReferenceCode: "TEST",
       billTo: {
-        firstName: "Node",
-        lastName: "V8",
-        street1: "6200 South 2370 East",
-        city: "Salt Lake City",
-        state: "UT",
-        postalCode: "84124",
+        firstName: "First",
+        lastName: "Last",
+        street1: "901 Metro Center Blvd",
+        city: "Foster City",
+        state: "CA",
+        postalCode: "94404",
         country: "US",
         email: "null@cybersource.com"
       },
+      item: {
+        $id: 0,
+        unitPrice: "5.00",
+        quantity: "5",
+        commodityCode: "123"
+      },
       purchaseTotals: {
         currency: "USD",
-        grandTotalAmount: 7.00
+        //grandTotalAmount: 7.00
       },
       card: {
         accountNumber: "4111111111111111",
